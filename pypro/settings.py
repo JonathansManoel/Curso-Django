74979a2de14db69fc1aa7ee11376f9c0fded8285
Jonathan da Silva Manoel <96275361+JonathansManoel@users.noreply.github.com>
2022-02-15 03:00:09 -0300

    Ajustando

--- pypro/settings.py
+++ pypro/settings.py
@@ -9,11 +9,9 @@ https://docs.djangoproject.com/en/4.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
-
+import os
 from pathlib import Path
-
-# Build paths inside the project like this: BASE_DIR / 'subdir'.
-from decouple import config
+from decouple import config, Csv
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
