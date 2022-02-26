90cc60cd791f107b2d29c6e689999a9225a37374
Jonathan da Silva Manoel <96275361+JonathansManoel@users.noreply.github.com>
2022-02-26 04:25:52 -0300

    Composição de Templates

--- pypro/base/tests/test_home.py
+++ pypro/base/tests/test_home.py
@@ -17,5 +17,9 @@ def test_title(resp):
     assert_contains(resp, '<title>Python Pro - Home</title>')
 
 
-def test_link(resp):
+def test_home_link(resp):
     assert_contains(resp, f'href="{reverse("base:home")}">Python Pro</a>')
+
+
+def test_email_link(resp):
+    assert_contains(resp, 'href="mailto:ramalho@python.pro.br"')
