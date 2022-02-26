22c10497a4b62a3c2aa7d57b0724307440ec794b
Jonathan da Silva Manoel <96275361+JonathansManoel@users.noreply.github.com>
2022-02-26 02:14:47 -0300

    Video de Acordo com Slug

--- pypro/aperitivos/views.py
+++ pypro/aperitivos/views.py
@@ -2,5 +2,9 @@ from django.shortcuts import render
 
 
 def video(request, slug):
-    video = {'titulo': 'Video Aperitivo: Motivação', 'vimeo_id': "681730350?h=a247ca87c2"}
+    videos = {
+        'motivacao':{'titulo': 'Video Aperitivo: Motivação', 'vimeo_id': "681730350?h=a247ca87c2"},
+        'instalacao-windows':{'titulo': 'Instalação Windows', 'vimeo_id': 251497668}
+    }
+    video = videos[slug]
     return render(request, 'aperitivos/video.html', context={'video': video})
