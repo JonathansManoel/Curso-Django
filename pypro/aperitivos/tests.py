7d07b25cdd72134375d6242e7976793a263d0dd6
Jonathan da Silva Manoel <96275361+JonathansManoel@users.noreply.github.com>
2022-02-26 02:07:18 -0300

    Contexto de Template

--- pypro/aperitivos/tests.py
+++ pypro/aperitivos/tests.py
@@ -10,11 +10,11 @@ def resp(client):
 
 
 def test_status_code(resp):
-    assert resp.status_code==200
+    assert resp.status_code == 200
 
 
 def test_titulo_video(resp):
-    assert_contains(resp, '<h1>Video Aperitivo: Motivação</h1>')
+    assert_contains(resp, 'Video Aperitivo: Motivação')
 
 
 def test_conteudo_video(resp):
--- pypro/aperitivos/views.py
+++ pypro/aperitivos/views.py
@@ -2,4 +2,5 @@ from django.shortcuts import render
 
 
 def video(request, slug):
-    return render(request, 'aperitivos/video.html')
+    video = {'titulo': 'Video Aperitivo: Motivação', 'vimeo_id': "681730350?h=a247ca87c2"}
+    return render(request, 'aperitivos/video.html', context={'video': video})
