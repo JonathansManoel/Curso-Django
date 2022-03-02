9836783efb31b4d4ea07fb1532f9d5cce63f9a62
Jonathan da Silva Manoel <96275361+JonathansManoel@users.noreply.github.com>
2022-03-02 05:47:24 -0300

    Admin com Modelos Relacionados

--- pypro/turmas/admin.py
+++ pypro/turmas/admin.py
@@ -4,8 +4,16 @@ from django.contrib import admin
 from pypro.turmas.models import Turma
 
 
+class MatriculaInlne(admin.TabularInline):
+    model = Turma.alunos.through
+    extra = 1
+    readonly_fields = ('data',)
+    autocomplete_fields = ('usuario',)
+    ordering = ('-data',)
+
 @admin.register(Turma)
 class TurmaAdmin(admin.ModelAdmin):
+    inlines = [MatriculaInlne]
     list_display = ('nome', 'slug', 'inicio', 'fim')
     prepopulated_fields = {'slug': ('nome',)}
     ordering = ('-inicio',)
